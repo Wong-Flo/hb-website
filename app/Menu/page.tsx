d54f930ac445adac9fcaf06b4e01a6103eba6b6f
Wong-Flo <florian.wong@outlook.com>
2025-02-11 22:34:09 +0100

    Add sections to menu/ and adding design for pdf,

--- app/Menu/page.tsx
+++ app/Menu/page.tsx
@@ -1,5 +1,29 @@
 import Navbar from '../Navbar/Navbar';
+import styles from './Menu.module.css';
 
 export default function Menu() {
-  return <Navbar titleText="this is the Menu Page" />;
+  return (
+    <>
+      <Navbar titleText="this is the Menu Page" />
+      <div className={styles.SectionContainer}>
+        <div className={styles.SectionLeft}>
+          <div className={styles.Kategorie}>
+            <span>Getränke</span>
+          </div>
+          <div className={styles.Kategorie}>
+            <span>Dim-Sum</span>
+          </div>
+          <div className={styles.Kategorie}>
+            <span>Vorspeisen</span>
+          </div>
+          <div className={styles.Kategorie}>
+            <span>Rice</span>
+          </div>
+          <div className={styles.Kategorie}>
+            <span>Hauptspeisen</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
