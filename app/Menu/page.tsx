220a0c4245e8db61d377fcd79331dd1d46bc4e76
Wong-Flo <florian.wong@outlook.com>
2025-02-12 20:55:40 +0100

    Add implementation of react-pdf

--- app/Menu/page.tsx
+++ app/Menu/page.tsx
@@ -1,14 +1,30 @@
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import { Viewer, Worker } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import Image from 'next/image';
 import Navbar from '../Navbar/Navbar';
 import styles from './Menu.module.css';
 
 export default function Menu() {
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
   return (
     <>
       <Navbar titleText="this is the Menu Page" />
+      <div className={styles.DivisionDivider}>
+        <Image
+          src="/StockPic6.jpg"
+          alt="Cartoon Picture of dinner Table and a Dim-sum Cart"
+          layout="fill"
+        />
+      </div>
       <div className={styles.SectionContainer}>
         <div className={styles.SectionLeft}>
           <div className={styles.Kategorie}>
             <span>Getränke</span>
+          </div>
+          <div className={styles.Kategorie}>
+            <span>Tagesmenü</span>
           </div>
           <div className={styles.Kategorie}>
             <span>Dim-Sum</span>
@@ -22,6 +38,19 @@ export default function Menu() {
           <div className={styles.Kategorie}>
             <span>Hauptspeisen</span>
           </div>
+          <div className={styles.Kategorie}>
+            <span>Nachspeisen</span>
+          </div>
+        </div>
+        <div className={styles.SectionRight}>
+          <Worker
+            workerUrl={`https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js`}
+          >
+            <Viewer
+              fileUrl="/Speisekarte.pdf"
+              plugins={[defaultLayoutPluginInstance]}
+            />
+          </Worker>
         </div>
       </div>
     </>
