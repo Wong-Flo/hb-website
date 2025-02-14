fc8cf39fddd7a91f98396f0fd3e3966a20c06341
Wong-Flo <florian.wong@outlook.com>
2025-02-14 16:18:22 +0100

    Add About Us page

---
+++ app/AboutUs/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutUs() {
+  return (
+    <>
+      <div>Das ist eine Seite Über uns</div>
+    </>
+  );
+}
--- app/Menu/page.tsx
+++ app/Menu/page.tsx
@@ -9,39 +9,6 @@ export default function Menu() {
       <Navbar titleText="This is the Menu Page" />
       <div className={styles.DivisionDivider}>
         <MenuTab />
-        {/*   <div className={styles.FoodCategoryTab}></div>
-         <Image
-          src="/StockPic6.jpg"
-          alt="dinner Table with different kinds of Dim-Sum"
-          layout="fill"
-          objectFit="cover"
-        />*/}
-      </div>
-      <div className={styles.SectionContainer}>
-        <div className={styles.SectionLeft}>
-          <div className={styles.Kategorie}>
-            <span>Getränke</span>
-          </div>
-          <div className={styles.Kategorie}>
-            <span>Tagesmenü</span>
-          </div>
-          <div className={styles.Kategorie}>
-            <span>Dim-Sum</span>
-          </div>
-          <div className={styles.Kategorie}>
-            <span>Vorspeisen</span>
-          </div>
-          <div className={styles.Kategorie}>
-            <span>Rice</span>
-          </div>
-          <div className={styles.Kategorie}>
-            <span>Hauptspeisen</span>
-          </div>
-          <div className={styles.Kategorie}>
-            <span>Nachspeisen</span>
-          </div>
-        </div>
-        <div className={styles.SectionRight}></div>
       </div>
     </>
   );
