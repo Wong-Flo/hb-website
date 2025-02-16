e6cc56a3a105f14216dc91eeae6f7b8e92eafe49
Wong-Flo <florian.wong@outlook.com>
2025-02-16 18:09:29 +0100

    Add iframe to layout for better display of page

--- app/layout.tsx
+++ app/layout.tsx
@@ -149,7 +149,16 @@ export default function RootLayout({
               *Last Order 30 minutes before closing*
             </p>
           </div>
-          <div className={styles.Address}>Mariahilfergürtel 17 | 1150 Wien</div>
+          <div className={styles.Address}>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5319.498618953853!2d16.33495227536371!3d48.19218144735952!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476da81f177a87af%3A0x886e3b9b7217b8fc!2sHappy%20Buddha!5e0!3m2!1sen!2sat!4v1738078554284!5m2!1sen!2sat"
+              width="100%"
+              height="450"
+              style={{ border: 0 }}
+              allowFullScreen
+              loading="lazy"
+            ></iframe>
+          </div>
         </div>
         <footer className={styles.FooterSection}>
           <Link href="/Impressum">
