1c11e7c0a77f279b86edb04cffffd3d8b971d467
Wong-Flo <florian.wong@outlook.com>
2025-02-27 22:35:57 +0100

    Add new file component for Contact section

---
+++ app/Contact/contact.tsx
@@ -0,0 +1,14 @@
+import styles from './contact.module.css';
+
+export default function ContactSection() {
+  return (
+    <div className={styles.ContactSection}>
+      <h2>Kontakt</h2>
+      <div>Mariahilfergürtel 17 </div> <div> 1150 Wien </div>
+      <div>+43-1-8934217</div>
+      <div>Mini Logo</div>
+      <div>Mini Logo</div>
+      <div>Mini Logo</div>
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import Link from 'next/link';
+import ContactSection from './Contact/contact';
 import styles from './layout.module.css';
 import OpeningTimes from './OpeningTimes/OpeningTimes';
 
@@ -31,11 +32,7 @@ export default function RootLayout({
         <main>{children}</main>
         <div className={styles.BottomSection}>
           <OpeningTimes />
-          <div className={styles.ContactSection}>
-            <h2>Kontakt</h2>
-            <div>Mariahilfergürtel 17 </div> <div> 1150 Wien </div>
-            <div>+43-1-8934217</div>
-          </div>
+          <ContactSection />
           <div className={styles.Address}>
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5319.498618953853!2d16.33495227536371!3d48.19218144735952!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476da81f177a87af%3A0x886e3b9b7217b8fc!2sHappy%20Buddha!5e0!3m2!1sen!2sat!4v1738078554284!5m2!1sen!2sat"
