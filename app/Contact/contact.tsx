8debf04377756d2b0bfec627ef5dca7cf1d59fac
Wong-Flo <florian.wong@outlook.com>
2025-02-28 21:09:15 +0100

    Add placement for logos

--- app/Contact/contact.tsx
+++ app/Contact/contact.tsx
@@ -6,9 +6,10 @@ export default function ContactSection() {
       <h2>Kontakt</h2>
       <div>Mariahilfergürtel 17 </div> <div> 1150 Wien </div>
       <div>+43-1-8934217</div>
-      <div>Mini Logo</div>
-      <div>Mini Logo</div>
-      <div>Mini Logo</div>
+      <div>Instagram Logo</div>
+      <div>facebook Logo</div>
+      <div>trip Logo</div>
+      <div>yelp Logo</div>
     </div>
   );
 }
