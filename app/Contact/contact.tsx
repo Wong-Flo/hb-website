c762571b41c84e6f37154a78c2c8b8e2b644c486
Wong-Flo <florian.wong@outlook.com>
2025-03-02 23:30:48 +0100

    Add icon for logo and link to specific website

--- app/Contact/contact.tsx
+++ app/Contact/contact.tsx
@@ -1,3 +1,6 @@
+import Link from 'next/link';
+import { FaGoogle, FaInstagram, FaTripadvisor } from 'react-icons/fa';
+import { FaMeta } from 'react-icons/fa6';
 import styles from './contact.module.css';
 
 export default function ContactSection() {
@@ -6,10 +9,36 @@ export default function ContactSection() {
       <h2>Kontakt</h2>
       <div>Mariahilfergürtel 17 </div> <div> 1150 Wien </div>
       <div>+43-1-8934217</div>
-      <div>Instagram Logo</div>
-      <div>facebook Logo</div>
-      <div>trip Logo</div>
-      <div>yelp Logo</div>
+      <div className={styles.LogoSection}></div>
+      <Link
+        href="https://www.instagram.com/happybuddha.wien/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaInstagram className={styles.Icon} />
+      </Link>
+      <Link
+        href="https://www.facebook.com/TheHappyBuddha"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaMeta className={styles.Icon} />
+      </Link>
+      <Link
+        href="https://www.tripadvisor.com/Restaurant_Review-g190454-d788083-Reviews-Happy_Buddha-Vienna.html"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaTripadvisor className={styles.Icon} />
+      </Link>
+      <Link
+        href="https://www.google.com/search?gs_ssp=eJzj4tZP1zcsSctOSTEwMGC0UjWoMDE3S0m0MEwzNDdPtDBPTLMyqLCwMEs1TrJMMjcyNE-ySEv24slILCioVEgqTUnJSAQAaiATbQ&q=happy+buddha&oq=happybuddh&gs_lcrp=EgZjaHJvbWUqEggCEC4YChivARjHARiABBiOBTIGCAAQRRg5MgYIARBFGDsyEggCEC4YChivARjHARiABBiOBTIJCAMQABgKGIAEMgkIBBAAGAoYgAQyBwgFEAAYgAQyFQgGEC4YChivARjHARiABBiYBRiaBTIPCAcQLhgKGK8BGMcBGIAEMg8ICBAuGAoYrwEYxwEYgAQyCQgJEAAYChiABNIBCTQxNjZqMGoxNagCCbACAfEFhwiQNW62X2o&sourceid=chrome&ie=UTF-8
+"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaGoogle className={styles.Icon} />
+      </Link>
     </div>
   );
 }
