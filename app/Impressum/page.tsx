6db4fd8eab2bd0d897e8f8f0d78de5adefb1d895
Wong-Flo <florian.wong@outlook.com>
2025-04-17 23:50:08 +0200

    rework

--- app/Impressum/page.tsx
+++ app/Impressum/page.tsx
@@ -5,19 +5,21 @@ export default function Impressum() {
   return (
     <>
       <Navbar titleText="Impressum" />
-      {/* <div className={styles.ImpressumContainer}>
-        <h3>Gem. §14 UGB und § 5 E-Commerce-Gesetz:</h3>
-        <strong>Firmenname:</strong> W57 Gesellschaft m.b.H
-        <br />
-        <strong>Rechtsform:</strong> Gesellschaft mit beschränkter Haftung
-        <br />
-        <strong>Sitz:</strong> Wien <br />
-        <strong>Kammer:</strong> Gastronomie, Fachgruppe Wien <br />
-        <strong>Umsatzsteuer-Identifikationsnummer:</strong> UID ATU 67715225
-        <br />
-        <strong>Firmenbuchnummer:</strong> FN 386694V <br />
-        <strong>Firmenbuchgericht:</strong> Handelsgericht Wien <br />
-      </div> */}
+      {
+        <div className={styles.ImpressumContainer}>
+          <h3>Gem. §14 UGB und § 5 E-Commerce-Gesetz:</h3>
+          <strong>Firmenname:</strong> W57 Gesellschaft m.b.H
+          <br />
+          <strong>Rechtsform:</strong> Gesellschaft mit beschränkter Haftung
+          <br />
+          <strong>Sitz:</strong> Wien <br />
+          <strong>Kammer:</strong> Gastronomie, Fachgruppe Wien <br />
+          <strong>Umsatzsteuer-Identifikationsnummer:</strong> UID ATU 67715225
+          <br />
+          <strong>Firmenbuchnummer:</strong> FN 386694V <br />
+          <strong>Firmenbuchgericht:</strong> Handelsgericht Wien <br />
+        </div>
+      }
     </>
   );
 }
